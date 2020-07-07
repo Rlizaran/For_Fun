e57b386dce2b9b473ab15fe6a632b1e6c688ccde
Rlizaran <67213261+Rlizaran@users.noreply.github.com>
2020-07-06 22:10:37 -0700

    Create For_Fun.py

---
+++ For_Fun.py
@@ -0,0 +1,11 @@
+""" def week(i):
+    switcher = {
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday', 
+    }
+    return switcher.get(i, 'Invalid day of week') 
+print(week(2))    """
+
+for i in range(10):
+    print(i)
