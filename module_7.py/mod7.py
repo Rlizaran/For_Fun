510802c01df217a3741bba10b685fdaec8b677dc
Rlizaran <67213261+Rlizaran@users.noreply.github.com>
2020-07-20 17:43:07 -0700

    Update mod7.py

--- module_7.py/mod7.py
+++ module_7.py/mod7.py
@@ -1,7 +1,16 @@
+""" 
 A = 'apples'
 B='appricots'
 
 print('A ord: B ord')
 for a_char, b_char in zip(A, B):
     print(f'{ord(a_char):<6}: {ord(b_char):>}')
+
+"""
+#check to see if A is in between 0 and 10
+""" 
+A = 10
+B = 5
+print(A % 2 == 0 and (not B % 2 == 0)) 
+"""
 
